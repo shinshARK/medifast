15b4431176c3e81e70acb82e8acc36655f6cfaee
RepanDh <lemonbuah1234@gmail.com>
2024-04-05 17:33:20 +0700

    nambah detail pertemuan

--- lib/screens/detail_pertemuan.dart
+++ lib/screens/detail_pertemuan.dart
@@ -1,33 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-
-// ignore: camel_case_types
-class detail_pertemuan extends StatefulWidget {
-  const detail_pertemuan({super.key});
+class DetailPertemuan extends StatefulWidget {
+  const DetailPertemuan({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
-  _detail_pertemuanState createState() => _detail_pertemuanState();
+  _DetailPertemuanState createState() => _DetailPertemuanState();
 }
 
-// ignore: camel_case_types
-class _detail_pertemuanState extends State<detail_pertemuan> {
+class _DetailPertemuanState extends State<DetailPertemuan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail',style: TextStyle(fontWeight: FontWeight.w700),),
+        title: const Text(
+          'Detail',
+          style: TextStyle(fontWeight: FontWeight.w700),
+        ),
         elevation: 10,
         shadowColor: Colors.white,
-        
       ),
-      // kalau mau nambahin const nya hapus aja 
-      body: const Column(
-          children: [
-            Text("detail")
-          ],
-      )
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+          _profil(context),
+          const Text(
+            "Dr. Eki Rakhmah Z., Sp.A",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const Text("Dokter Anak"),
+          const SizedBox(height: 25), // Menambahkan ruang kosong
+          Container(
+            padding: const EdgeInsets.only(
+                left: 30.0), // Menambahkan ruang di sebelah kiri
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Rata kiri
+              children: [
+                Text("Nomor Antrian: 12"),
+                Text("Ruanggan: Gedung A, Lantai 3, Ruangan 305"),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20), // Menambahkan ruang kosong
+          Image.asset('assets/images/qrcode.png'), // Menambahkan gambar
+        ],
+      ),
     );
   }
 
+  Stack _profil(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: SizedBox(
+            width: 144.0,
+            height: 144.0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 52,
+                backgroundColor: Colors.white.withOpacity(0.0),
+                child: const Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage('assets/images/dokter_1.png'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
