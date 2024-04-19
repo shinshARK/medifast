c956a9102ea92191a2ea2c382039c957aae681e2
adrianKiv <adrianmulianto@gmail.com>
2024-04-19 16:18:33 +0700

    db medifast

---
+++ db_medifast.sql
@@ -0,0 +1,72 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : coba01
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : db_medifast
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2024-04-19 16:18:02
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `artikel`
+-- ----------------------------
+DROP TABLE IF EXISTS `artikel`;
+CREATE TABLE `artikel` (
+  `id_artikel` int(11) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(255) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `isi` varchar(255) DEFAULT NULL,
+  `createdat` date DEFAULT NULL,
+  `updatedat` date DEFAULT NULL,
+  PRIMARY KEY (`id_artikel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Records of artikel
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `dokter`
+-- ----------------------------
+DROP TABLE IF EXISTS `dokter`;
+CREATE TABLE `dokter` (
+  `id_dokter` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_dokter` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `spesialis` varchar(255) DEFAULT NULL,
+  `rating` varchar(255) DEFAULT NULL,
+  `favorited` int(10) DEFAULT NULL,
+  `pengalaman` int(10) DEFAULT NULL,
+  `tentang` varchar(255) DEFAULT NULL,
+  `jadwal` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_dokter`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Records of dokter
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telephone` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
