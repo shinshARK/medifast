bc64b93bab4beef91a144549ebe462636e6ffccb
shinshARK <137866950+shinshARK@users.noreply.github.com>
2024-04-21 11:36:18 +0700

    test: get article from api

--- lib/models/artikel_models.dart
+++ lib/models/artikel_models.dart
@@ -1,44 +1,60 @@
-class ArtikelModel{
-  String judul;
-  String foto;
-  String isi;
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
+
+class Article{
+  String title;
+  String photo;
+  String content;
   DateTime createdat;
   DateTime updatedat;
 
 
-  ArtikelModel({
-    required this.judul,
-    required this.foto,
-    required this.isi,
+  Article({
+    required this.title,
+    required this.photo,
+    required this.content,
     required this.createdat,
     required this.updatedat
   });
 
+  factory Article.fromJson(Map<String, dynamic> json) {
+    DateFormat format = DateFormat('yyyy-MM-dd');
+    return Article(
+      title: json['title'],
+      photo: json['photo'],
+      content: json['content'].toString().replaceAll('\\n', '\n'),
+      createdat: format.parse(json['createdat']),
+      updatedat: format.parse(json['updatedat']),
+    );
+  }
 }
 
 
-// ignore: non_constant_identifier_names
-var data_artikel = [
-  // Data yang sudah ada
-  ArtikelModel(judul: "judul1 ", foto: "a.png", isi: "isi1", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  ArtikelModel(judul: "judul2", foto: "b.png", isi: "isi2", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  ArtikelModel(judul: "judul3", foto: "c.png", isi: "isi3", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  ArtikelModel(judul: "judul4", foto: "d.png", isi: "isi4", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  ArtikelModel(judul: "judul5", foto: "e.png", isi: "isi5", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23))
 
-  // ArtikelModel(judul: "judul6", foto: "f.png", isi: "isi6", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul7", foto: "g.png", isi: "isi7", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul8", foto: "h.png", isi: "isi8", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul9", foto: "i.png", isi: "isi9", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul10", foto: "j.png", isi: "isi10", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul11", foto: "k.png", isi: "isi11", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul12", foto: "l.png", isi: "isi12", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul13", foto: "m.png", isi: "isi13", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul14", foto: "n.png", isi: "isi14", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul15", foto: "o.png", isi: "isi15", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul16", foto: "p.png", isi: "isi16", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul17", foto: "q.png", isi: "isi17", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul18", foto: "r.png", isi: "isi18", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul19", foto: "s.png", isi: "isi19", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-  // ArtikelModel(judul: "judul20", foto: "t.png", isi: "isi20", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
-];
+
+// // ignore: non_constant_identifier_names
+// var data_artikel = [
+//   // Data yang sudah ada
+//   Article(title: "title1 ", photo: "a.png", content: "content1", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   Article(title: "title2", photo: "b.png", content: "content2", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   Article(title: "title3", photo: "c.png", content: "content3", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   Article(title: "title4", photo: "d.png", content: "content4", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   Article(title: "title5", photo: "e.png", content: "content5", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23))
+
+//   // Article(title: "title6", photo: "f.png", content: "content6", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title7", photo: "g.png", content: "content7", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title8", photo: "h.png", content: "content8", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title9", photo: "i.png", content: "content9", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title10", photo: "j.png", content: "content10", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title11", photo: "k.png", content: "content11", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title12", photo: "l.png", content: "content12", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title13", photo: "m.png", content: "content13", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title14", photo: "n.png", content: "content14", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title15", photo: "o.png", content: "content15", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title16", photo: "p.png", content: "content16", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title17", photo: "q.png", content: "content17", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title18", photo: "r.png", content: "content18", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title19", photo: "s.png", content: "content19", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+//   // Article(title: "title20", photo: "t.png", content: "content20", createdat: DateTime.now(), updatedat: DateTime(2024, 5, 23)),
+// ];
