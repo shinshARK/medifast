8701ca9fa7b55a104a165dc97e1e3ec974b55976
Ilham9675 <ilhamakbar9675@gmail.com>
2024-04-21 18:07:54 +0700

    refactor page artikel & detail artikel agar mengambil dari api

--- lib/screens/halamanArtikel.dart
+++ lib/screens/halamanArtikel.dart
@@ -3,6 +3,10 @@ import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rumah_sakit/components/bottomNavigasiBar.dart';
 import 'package:rumah_sakit/screens/detailArtikel2.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart'; // Import DateFormat
+import 'package:rumah_sakit/models/artikel_models.dart';
 
 // ignore: must_be_immutable, camel_case_types
 class halamanArtikel extends StatefulWidget {
@@ -15,6 +19,37 @@ class halamanArtikel extends StatefulWidget {
 
 // ignore: camel_case_types
 class _halamanArtikelState extends State<halamanArtikel> {
+  List<Article> articles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    // Fetch data from the API
+    final response = await http.get(Uri.parse('http://127.0.0.1:8000/articles'));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the JSON
+      List<dynamic> jsonResponse = json.decode(response.body);
+      // print(jsonResponse);
+
+      // Convert each JSON object to an Article object
+      List<Article> parsedArticles = jsonResponse.map((articleJson) {
+        return Article.fromJson(articleJson);
+      }).toList();
+
+      // Update the state with the parsed articles
+      setState(() {
+        articles = parsedArticles;
+      });
+    } else {
+      // If the server did not return a 200 OK response, throw an error.
+      throw Exception('Failed to load articles');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +78,7 @@ class _halamanArtikelState extends State<halamanArtikel> {
                   separatorBuilder: (context, index) => const SizedBox(
                     width: 20.0,
                   ),
-                  itemCount: 1,
+                  itemCount: 2,
                 ),
               ),
               _artikel2(),
@@ -52,11 +87,11 @@ class _halamanArtikelState extends State<halamanArtikel> {
                 margin: const EdgeInsets.only(left: 15, right: 10),
                 child: ListView.separated(
                   scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => _artikel2_Padding(),
+                  itemBuilder: (context, index) => _artikel2_Padding(index),
                   separatorBuilder: (context, index) => const SizedBox(
                     width: 20.0,
                   ),
-                  itemCount: 1,
+                  itemCount: articles.length,
                 ),
               ),
               _artikel3(),
@@ -69,7 +104,7 @@ class _halamanArtikelState extends State<halamanArtikel> {
                   separatorBuilder: (context, index) => const SizedBox(
                     width: 20.0,
                   ),
-                  itemCount: 1,
+                  itemCount: 2,
                 ),
               ),
             ],
@@ -91,7 +126,7 @@ class _halamanArtikelState extends State<halamanArtikel> {
         alignment: Alignment.center,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
-            color: Color.fromRGBO(102, 121, 105, 30)),
+            color: const Color.fromRGBO(102, 121, 105, 30)),
         child: Row(
           children: [
             Container(
@@ -116,12 +151,12 @@ class _halamanArtikelState extends State<halamanArtikel> {
     );
   }
 
-  GestureDetector _artikel2_Padding() {
+  GestureDetector _artikel2_Padding(int index) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => detailArtikel2()),
+          MaterialPageRoute(builder: (context) => detailArtikel2(articles: articles[index],judulpage: "Kesehatan",)),
         );
       },
       child: Container(
@@ -131,21 +166,21 @@ class _halamanArtikelState extends State<halamanArtikel> {
         alignment: Alignment.center,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
-            color: Color.fromRGBO(102, 121, 105, 30)),
+            color: const Color.fromRGBO(102, 121, 105, 30)),
         child: Row(
           children: [
             Container(
               width: 180.0,
               height: 140.0,
               alignment: Alignment.bottomCenter,
-              child: Image.asset('assets/images/artikel2.png'),
+              child: Image.asset('assets/images/${articles[index].photo}'),
             ),
             Container(
               width: 178.0,
               height: 162.0,
               padding: const EdgeInsets.only(left: 20, top: 40, bottom: 10),
-              child: const Text('4 Rekomendasi Obat Maag yang ampuh',
-                  style: TextStyle(
+              child: Text(articles[index].title,
+                  style: const TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.w700,
                       fontSize: 16)),
@@ -166,7 +201,7 @@ class _halamanArtikelState extends State<halamanArtikel> {
         alignment: Alignment.center,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
-            color: Color.fromRGBO(102, 121, 105, 30)),
+            color: const Color.fromRGBO(102, 121, 105, 30)),
         child: Row(
           children: <Widget>[
             Expanded(
