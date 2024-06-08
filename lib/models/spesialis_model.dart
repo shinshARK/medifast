5d0a03f698ccf021dca6019e293bddb9d285085e
adrianKiv <adrianmulianto@gmail.com>
2024-06-08 23:15:18 +0700

    buat model

---
+++ lib/models/spesialis_model.dart
@@ -0,0 +1,20 @@
+class SpesialisModel{
+  String namaspesialis;
+
+
+  SpesialisModel({
+    required this.namaspesialis,
+  });
+
+}
+
+var data_shift = [
+    SpesialisModel(namaspesialis: "anak"),
+    SpesialisModel(namaspesialis: "umum"),
+    SpesialisModel(namaspesialis: "dalam"),
+    SpesialisModel(namaspesialis: "bedah"),
+    SpesialisModel(namaspesialis: "mata"),
+    SpesialisModel(namaspesialis: "saraf"),
+    SpesialisModel(namaspesialis: "urologi"),
+    SpesialisModel(namaspesialis: "patologi"),
+  ];
